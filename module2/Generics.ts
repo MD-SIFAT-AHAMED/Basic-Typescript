e01ee051b5269c9d6d4325a81542d6690ad783d6
MD SIFAT AHAMED~ <mdsifat978@gmail.com>
2025-08-13 00:04:46 +0600

    Learn Generics

--- module2/Generics.ts
+++ module2/Generics.ts
@@ -1,8 +1,45 @@
 {
 
-// je kono somoy amra type toiri korte pari avong reuse korte pari tai holo generic type data
+    // je kono somoy amra type toiri korte pari avong reuse korte pari tai holo generic type data
 
-//Genecric type
+    //Generic type
+
+    type GenericArray<T> = Array<T>;
+
+    // const rollNumer: number[] = [10, 20, 30];
+    const rollNumer: GenericArray<number> = [10, 20, 30];
+
+
+    // const mentors: string[] = ['mr-x', 'ms-y'];
+    const mentors: GenericArray<string> = ['mr-x', 'ms-y'];
+
+
+    // const boolArray: boolean[] = [true, false, true]
+    const boolArray: GenericArray<boolean> = [true, false, true]
+
+
+    // array of object er khetre object params pathabo na , Borong argument declar kore dibo type
+
+    const user: GenericArray<{ name: string, age: number }> = [
+        {
+            name: "soifat",
+            age: 20
+
+        },
+        {
+            name: "sajid",
+            age: 48
+        }
+    ]
+
+
+    // Generic tuple
+
+    type GenericTuple<X, Y> = [X, Y]
+
+    // const manus: [string, string] = ['sifat', 'rofiq']
+    const manus: GenericTuple<string, string> = ['sifat', 'rofiq']
+    const userWithEmail: GenericTuple<number, { name: string, email: string }> = [13934, { name: "sifat", email: "a@gmail.com" }]
 
 
 }

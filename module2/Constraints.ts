504f8e4c82de1b37350b065bec98389aa4ba02aa
MD SIFAT AHAMED~ <mdsifat978@gmail.com>
2025-08-13 18:40:54 +0600

    constraints

---
+++ module2/Constraints.ts
@@ -0,0 +1,19 @@
+{
+
+    // conostraints 
+    //Mane holo kono kichu enforch kora je thaktei hobe
+
+    const AddCourseToStudent = <T extends { id: number, name: string, email: string }>(student: T) => {
+        const course = "Hello world";
+        return {
+            ...student,
+            course
+        }
+    }
+
+    const Stduent1 = AddCourseToStudent({ id: 89, name: "sifat ahamed", email: "z@gmail.com", isStatus: 'hi' })
+
+
+
+
+}
